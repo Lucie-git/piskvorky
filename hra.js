17fb39d02e717845ec4e9fb06e861cd340ca2126
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-23 16:40:25 +0200

    add aiMove fetch

--- hra.js
+++ hra.js
@@ -3,6 +3,23 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 let currentPlayer = 'circle';
 const playerImg = document.querySelector('.hraje__obrazek');
 
+const aiMove = async (board) => {
+  const response = await fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      board: board,
+      player: 'x',
+    }),
+  });
+  const data = await response.json();
+  const index = data.position.x + data.position.y * 10;
+  const gameButtons = document.querySelectorAll('.grid__item');
+  gameButtons[index].click();
+};
+
 const player = (event) => {
   event.target.disabled = true;
   if (currentPlayer === 'circle') {
@@ -31,6 +48,20 @@ const player = (event) => {
       alert(`Vyhrál hráč se symbolem ${winner}.`);
       location.reload();
     }, 150);
+  } else if (currentPlayer === 'cross') {
+    setTimeout(() => {
+      const updatedButtons = document.querySelectorAll('.grid__item');
+      const updatedBoard = Array.from(updatedButtons).map((button) => {
+        if (button.classList.contains('board__field--circle')) {
+          return 'o';
+        } else if (button.classList.contains('board__field--cross')) {
+          return 'x';
+        } else {
+          return '_';
+        }
+      });
+      aiMove(updatedBoard);
+    }, 300);
   }
 };
 
